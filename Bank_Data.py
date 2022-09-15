e1c01e29cf95eeb411b3e8a0df87fb9e28c47f96
Noob-boy <93995876+noob-bo-y@users.noreply.github.com>
2022-09-15 13:47:01 +0530

    Add files via upload

---
+++ Bank_Data.py
@@ -0,0 +1,12 @@
+
+# import requests
+
+# Ifsc_code = 'UBIN0573663'
+
+# API_fatch = 'https://ifsc.razorpay.com/'
+
+# Bank_data = requests.get(API_fatch + Ifsc_code).json()
+
+# print(Bank_data)
+
+print(627 % 45)
