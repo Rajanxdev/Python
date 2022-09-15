564383410f8f6bbe8c1cf46f22f6e0e5fe79c731
Noob-boy <93995876+noob-bo-y@users.noreply.github.com>
2022-09-15 14:14:59 +0530

    Update Bank_Data.py

--- Bank_Data.py
+++ Bank_Data.py
@@ -1,7 +1,7 @@
 # Subscribe my youtube channel 
  import requests
 
- Ifsc_code = 'UBIN0573663'  #Change IFSC Code as per your requirement
+ Ifsc_code = 'PUNB0102010'  #Change IFSC Code as per your requirement
 
  API_fatch = 'https://ifsc.razorpay.com/'
 
